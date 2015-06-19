74da9dcb764643599f25ab8913a338d4ad104c92
anphung <an.phung@hde.co.jp>
2015-06-19 10:50:23 +0900

    Initial Commit

---
+++ WatchActivitySample WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchActivitySample WatchKit Extension
+//
+//  Created by An Phung on 6/19/15.
+//  Copyright © 2015 HDE, Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
